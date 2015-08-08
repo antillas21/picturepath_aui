fa7be14fdc0d2c3078c6adadae38be644ddc9c51
Antonio Antillon <antillas21@gmail.com>
2015-08-08 13:27:37 -0700

    Adds specs for #check and #submit methods on PicturepathAUI::Client

--- lib/picturepath_aui/client.rb
+++ lib/picturepath_aui/client.rb
@@ -1,6 +1,9 @@
+require 'cgi'
+require 'open-uri'
+
 module PicturepathAUI
   class Client
-    attr_reader :api_version
+    attr_reader :api_version, :username, :password
 
     def initialize(attributes = {})
       @username = attributes.fetch(:username)
@@ -9,9 +12,36 @@ module PicturepathAUI
     end
 
     def check(payload)
+      post_request(:check, payload)
     end
 
     def submit(payload)
+      post_request(:submit, payload)
+    end
+
+    private
+
+    def escape(attribute)
+      return CGI.escape(attribute)
+    end
+
+    def post_request(action, payload)
+      url = (api_url + aui_default_params(action) + payload_params(payload)).to_s
+      return open(url).read
+    end
+
+    def api_url
+      "http://picturepath.homestore.com/picturepath/cgi-bin/receiver.pl"
+    end
+
+    def aui_default_params(action)
+      action_string = action.to_s.capitalize
+      "?username=#{escape(username)}&password=#{escape(password)}&client=AUI&" +
+      "version=#{api_version}&action=#{action_string}"
+    end
+
+    def payload_params(payload)
+      "&xml=#{escape(payload)}"
     end
   end
 end
--- spec/picturepath_aui/client_spec.rb
+++ spec/picturepath_aui/client_spec.rb
@@ -11,25 +11,45 @@ RSpec.describe PicturepathAUI::Client do
       expect { PicturepathAUI::Client.new(username: "username") }.to raise_error(
         KeyError)
     end
+
+    describe 'URL safe params' do
+      let(:client) { PicturepathAUI::Client.new(username: "a username", password: "has a password") }
+
+      it 'escapes username to be valid when passed as URL params' do
+        expect(client.send(:escape, client.username)).to eq("a+username")
+      end
+
+      it 'escapes password to be valid when passed as URL params' do
+        expect(client.send(:escape, client.password)).to eq("has+a+password")
+      end
+    end
   end
 
   describe '#check' do
     let(:client) { PicturepathAUI::Client.new(username: "username", password: "password") }
+    let(:payload) { File.read('./spec/fixtures/request.xml') }
 
     it 'requires a payload' do
       expect { client.check }.to raise_error(ArgumentError)
     end
 
-    it 'performs a :check request to AUI API'
+    it 'performs a :check request to AUI API' do
+      expect(client).to receive(:post_request).with(:check, payload)
+      client.check(payload)
+    end
   end
 
   describe '#submit' do
     let(:client) { PicturepathAUI::Client.new(username: "username", password: "password") }
+    let(:payload) { File.read('./spec/fixtures/request.xml') }
 
     it 'requires a payload' do
       expect { client.submit }.to raise_error(ArgumentError)
     end
 
-    it 'performs a :submit request to AUI API'
+    it 'performs a :submit request to AUI API' do
+      expect(client).to receive(:post_request).with(:submit, payload)
+      client.submit(payload)
+    end
   end
 end
